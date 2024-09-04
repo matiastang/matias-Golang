b464d1b89f53db6bf941cc37452ed4e92246835c
唐道勇 <matias@tangdaoyongdeMacBook-Pro.local>
2024-09-04 17:52:47 +0800

    feat: - 添加编译测试逃逸分析

--- learn/test/main.go
+++ learn/test/main.go
@@ -2,18 +2,25 @@
  * @Author: matiastang
  * @Date: 2022-04-19 19:46:34
  * @LastEditors: matiastang
- * @LastEditTime: 2022-04-19 19:46:35
+ * @LastEditTime: 2024-09-04 17:51:49
  * @FilePath: /matias-Golang/learn/test/main.go
  * @Description:
  */
+/*
+* go tool compile -m ./main.go
+* 编译成可执行文件，生成main.o
+ */
 package main
 
-import "fmt"
+// import "fmt"
 
-func init() {
-	fmt.Println("learn main init")
-}
+// func init() {
+// 	fmt.Println("learn main init")
+// }
 
 func main() {
-	fmt.Println("learn main main")
+	// fmt.Println("learn main main")
+
+	data := make(map[string][]string)
+	data["key"] = []string{"value1"}
 }
